1cd1ebb8ddd55780dfa3ee336a53ea44373f8ea4
Infer-Noam <xd.NNoam39@idf.il>
2025-07-17 10:36:01 +0300

    Updated recipe

--- frontend/src/components/recipe/recipe.tsx
+++ frontend/src/components/recipe/recipe.tsx
@@ -1,6 +1,10 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { Box, Typography } from "@mui/material";
+import type { FC } from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
 
 type RecipeProps = {
   recipeName: string;
@@ -8,7 +12,12 @@ type RecipeProps = {
   creationDate: Date;
   steps: string[];
 };
-export const Recipe = () => {
+export const Recipe: FC<RecipeProps> = (props) => {
+  const nowSeconds = new Date().getSeconds();
+  const createdSeconds = props.creationDate.getSeconds();
+  const seconds = createdSeconds - nowSeconds;
+  const postText = `Posted ${seconds}s ago`;
+
   return (
     <Box sx={{ minWidth: 275 }}>
       <Card variant="outlined">
@@ -17,18 +26,15 @@ export const Recipe = () => {
             gutterBottom
             sx={{ color: "text.secondary", fontSize: 14 }}
           >
-            Word of the Day
+            {props.chefName}
           </Typography>
           <Typography variant="h5" component="div">
-            be
+            {props.recipeName}
           </Typography>
+
+          <Typography variant="body2">{props.steps}</Typography>
           <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
-            adjective
-          </Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
+            {postText}
           </Typography>
         </CardContent>
       </Card>
--- frontend/src/pages/home/homePage.tsx
+++ frontend/src/pages/home/homePage.tsx
@@ -1,10 +1,17 @@
 import { Recipe } from "../../components/recipe/recipe";
 
+const yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
 
 const HomePage = () => {
   return (
     <div>
-      <Recipe />
+      <Recipe
+        recipeName="Cake"
+        chefName="Noam Naor"
+        creationDate={yesterday}
+        steps={[]}
+      />
     </div>
   );
 };
