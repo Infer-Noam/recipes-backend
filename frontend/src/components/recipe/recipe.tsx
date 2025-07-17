03fbf02254c1ed64b1e56ce0b044a636551e3e7d
Infer-Noam <xd.NNoam39@idf.il>
2025-07-17 13:29:38 +0300

    Added recipe component

--- frontend/src/components/recipe/recipe.tsx
+++ frontend/src/components/recipe/recipe.tsx
@@ -1,43 +1,115 @@
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import { Box, Typography } from "@mui/material";
-import type { FC } from "react";
-import Accordion from "@mui/material/Accordion";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import AccordionSummary from "@mui/material/AccordionSummary";
-
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Collapse,
+  Avatar,
+  Typography,
+  IconButton,
+  type IconButtonProps,
+} from "@mui/material";
+import { blue } from "@mui/material/colors";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 type RecipeProps = {
   recipeName: string;
   chefName: string;
   creationDate: Date;
   steps: string[];
+  description: string,
+  imageUrl: string
 };
-export const Recipe: FC<RecipeProps> = (props) => {
-  const nowSeconds = new Date().getSeconds();
-  const createdSeconds = props.creationDate.getSeconds();
-  const seconds = createdSeconds - nowSeconds;
-  const postText = `Posted ${seconds}s ago`;
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: "rotate(0deg)",
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: "rotate(180deg)",
+      },
+    },
+  ],
+}));
+
+export const Recipe: React.FC<RecipeProps> = (props) => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
-    <Box sx={{ minWidth: 275 }}>
-      <Card variant="outlined">
-        <CardContent>
-          <Typography
-            gutterBottom
-            sx={{ color: "text.secondary", fontSize: 14 }}
-          >
-            {props.chefName}
-          </Typography>
-          <Typography variant="h5" component="div">
-            {props.recipeName}
-          </Typography>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: blue[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.recipeName}
+        subheader={`By ${props.chefName}`}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={props.imageUrl}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {props.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <Typography variant="body2" sx={{}}>
+          {props.creationDate.toDateString()}
+        </Typography>
 
-          <Typography variant="body2">{props.steps}</Typography>
-          <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
-            {postText}
-          </Typography>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography sx={{ marginBottom: 2 }}>Method:</Typography>
+          {props.steps.map((step, index) => (
+            <Typography key={index} sx={{ marginBottom: 2 }}>
+              {step}
+            </Typography>
+          ))}
         </CardContent>
-      </Card>
-    </Box>
+      </Collapse>
+    </Card>
   );
 };
--- frontend/src/pages/home/homePage.tsx
+++ frontend/src/pages/home/homePage.tsx
@@ -1,18 +1,62 @@
 import { Recipe } from "../../components/recipe/recipe";
+import { Box, Grid } from "@mui/material";
 
-const yesterday = new Date();
-yesterday.setDate(yesterday.getDate() - 1);
-
+const recipes = [
+  {
+    uuid: "x",
+    recipeName: "Cake",
+    chefName: "Noam Naor",
+    creationDate: new Date(),
+    steps: [
+      "Break 3 eggs in a bowl",
+      "Add olive oil",
+      "Turn the heat to 6",
+      "Wait about 5 minutes",
+      "Bonn appetit",
+    ],
+    imageUrl:
+      "https://ts2.mm.bing.net/th?id=OIP.Y0-ld4ksP_ZxfdyMl6OW2QHaE8&pid=15.1",
+    descreption: `This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.`,
+  },
+  {
+    uuid: "x2",
+    recipeName: "Finland",
+    chefName: "Porto erpo",
+    creationDate: new Date(),
+    steps: [
+      "Break away from sweden using any means accessible",
+      "Great! Now secretly fortify your eastern border",
+      "Quick the russian ae invading. Get into your positions!",
+      "Wait about 5 minutes",
+      "KILL THEM ALL",
+      "ESTABLISH SUOMI!!!",
+    ],
+    imageUrl:
+      "https://th.bing.com/th/id/ODL.c6d1896131f7b0227b53923f19f0e5b3?w=156&h=95&c=10&rs=1&o=6&dpr=1.1&pid=AlgoBlockDebug",
+    descreption: `W finland`,
+  },
+];
 const HomePage = () => {
   return (
-    <div>
-      <Recipe
-        recipeName="Cake"
-        chefName="Noam Naor"
-        creationDate={yesterday}
-        steps={[]}
-      />
-    </div>
+    <Box>
+      <Grid container rowSpacing={2.5} columnSpacing={3.5}>
+        {[...recipes, ...recipes, ...recipes].map((recipe) => (
+          <Grid>
+            <Recipe
+              key={recipe.uuid}
+              recipeName={recipe.recipeName}
+              chefName={recipe.chefName}
+              creationDate={recipe.creationDate}
+              steps={recipe.steps}
+              description={recipe.descreption}
+              imageUrl={recipe.imageUrl}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
